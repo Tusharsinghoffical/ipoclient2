4049be295a33d310351b187fbead41304c60e5d2
Tushar Singh <tusharsinghkumar04@gmail.com>
2025-08-05 15:13:58 +0530

    Fix: static config for Render deployment

--- ipo_project/settings.py
+++ ipo_project/settings.py
@@ -54,9 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'ipo_project.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
